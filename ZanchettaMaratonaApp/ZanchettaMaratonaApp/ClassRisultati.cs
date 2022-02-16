944c8b23e63e86e4bd4e9df074e8aaf04e3f586e
Mattia <mattia.zanchetta2004@gmail.com>
2022-02-16 13:58:52 +0100

    scritte le classi e le funzioni

---
+++ ZanchettaMaratonaApp/ZanchettaMaratonaApp/ClassRisultati.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZanchettaMaratonaApp
+{
+    class ClassRisultati
+    {
+        public List<ClassRisultato> ListaRisultati { get; set; }
+
+        public ClassRisultati()
+        {
+            ListaRisultati = new List<ClassRisultato>();
+        }
+
+        public void AggiungiLinea(ClassRisultato risultato)
+        {
+            ListaRisultati.Add(risultato);
+        }
+    }
+    }
